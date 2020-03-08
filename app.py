04ca70ab2a48b082d291ff924c247b12e9f663dd
Noah Drisort <xojziyfay@gmail.com>
2020-03-08 21:53:22 +0700

    add demo input page

--- app.py
+++ app.py
@@ -6,16 +6,17 @@ app = Flask(__name__, static_url_path='/static')
 
 @app.route("/", methods=['GET', 'POST'])
 def main():
-    return render_template('create.html')
+    return render_template('index.html')
 
 
 
-@app.route("/demo" , methods=['GET', 'POST'])
-def demo():
+@app.route("/demo_input" , methods=['GET', 'POST'])
+def demo_input():
     if request.method == 'POST':
-        return render_template('demo.html')
+        return render_template('demo_audio.html')
     else:
-        return render_template('demo.html')
+        return render_template('demo_input.html')
+        #return "Nhan"
 
 
 
